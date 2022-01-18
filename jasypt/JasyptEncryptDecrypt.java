8c6ecf6e0e70834148de03851801a2a3a703f134
puneet.singhania <puneet.singhania@ril.com>
2022-01-18 15:30:25 +0530

    helper codes for apache and/or encryption

---
+++ jasypt/JasyptEncryptDecrypt.java
@@ -0,0 +1,14 @@
+package jasypt;
+
+public class JasyptEncryptDecrypt {
+
+    public static void main(String[] args) {
+        /*StandardPBEStringEncryptor e = new StandardPBEStringEncryptor();
+        e.setAlgorithm("PBEWITHSHA1ANDDESEDE");
+        e.setStringOutputType("hexadecimal");
+        e.setProviderName("SunJCE");
+        e.setPassword("ajiodod_uat@12345");
+        System.out.println("decrypted: "
+                + e.decrypt("C634B50AD07CA141C47FA56B2A9AC22B"));*/
+    }
+}
---
+++ multiThreadUdemy/BarrierProg.java
@@ -0,0 +1,88 @@
+package multiThreadUdemy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BarrierProg {
+
+    public static class Barrier {
+
+        private final int numberOfWorkers;
+        private Semaphore semaphore = new Semaphore(0);
+        private int counter = 0;
+        private Lock lock = new ReentrantLock();
+
+        public Barrier(int numberOfWorkers) {
+            this.numberOfWorkers = numberOfWorkers;
+        }
+
+        public void barrier() {
+            lock.lock();
+            boolean isLastWorker = false;
+            try {
+                counter++;
+
+                if (counter == numberOfWorkers) {
+                    isLastWorker = true;
+                }
+            } finally {
+                lock.unlock();
+            }
+
+            if (isLastWorker) {
+                semaphore.release(numberOfWorkers-1);
+            } else {
+                try {
+                    semaphore.acquire();
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+
+    public static class CoordinatedWorkRunner implements Runnable {
+        private Barrier barrier;
+
+        public CoordinatedWorkRunner(Barrier barrier) {
+            this.barrier = barrier;
+        }
+
+        @Override
+        public void run() {
+            try {
+                task();
+            } catch (InterruptedException e) {
+            }
+        }
+
+        private void task() throws InterruptedException {
+            // Performing Part 1
+            System.out.println(Thread.currentThread().getName()
+                    + " part 1 of the work is finished");
+
+            barrier.barrier();
+
+            // Performing Part2
+            System.out.println(Thread.currentThread().getName()
+                    + " part 2 of the work is finished");
+        }
+    }
+
+    public static void main(String [] args){
+        int numberOfThreads = 200; //or any number you'd like
+
+        List<Thread> threads = new ArrayList<>();
+
+        Barrier barrier = new Barrier(numberOfThreads);
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads.add(new Thread(new CoordinatedWorkRunner(barrier)));
+        }
+
+        for(Thread thread: threads) {
+            thread.start();
+        }
+    }
+}
